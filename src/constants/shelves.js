c6bcd9dd9040e711189c96582d3410a6428dd7f5
Sylvan Le Deunff <sylvan.le-deunff@ovhcloud.com>
2024-03-19 12:37:11 +0100

    Improve shopping list design

--- src/constants/shelves.js
+++ src/constants/shelves.js
@@ -1,42 +1,42 @@
-export default [
-    {
-        id: "bakery",
+export default {
+    "bakery": {
         title: "Boulangerie",
         icon: "mdi-baguette",
     },
-    {
-        id: "poultry",
-        title: "Volaille",
-        icon: "mdi-food-drumstick",
-    },
-    {
-        id: "cheese",
-        title: "Fromage",
-        icon: "mdi-cheese",
-    },
-    {
-        id: "fish",
-        title: "Poissonnerie",
-        icon: "mdi-fish",
-    },
-    {
-        id: "vegetables",
-        title: "Fruits & Légumes",
-        icon: "mdi-fruit-grapes",
-    },
-    {
-        id: "sausage",
-        title: "Charcuterie",
-        icon: "mdi-sausage",
-    },
-    {
-        id: "butcher",
+    "butcher": {
         title: "Boucherie",
         icon: "mdi-food-steak",
     },
-    {
-        id: "grocery",
+    "cheese": {
+        title: "Fromage",
+        icon: "mdi-cheese",
+    },
+    "fish": {
+        title: "Poissonnerie",
+        icon: "mdi-fish",
+    },
+    "grocery": {
         title: "Epicerie",
         icon: "mdi-barley"
-    }
-];
+    },
+    "patisserie": {
+        title: "Patisserie",
+        icon: "mdi-cupcake",
+    },
+    "poultry": {
+        title: "Volaille",
+        icon: "mdi-food-drumstick",
+    },
+    "sausage": {
+        title: "Charcuterie",
+        icon: "mdi-sausage",
+    },
+    "spices": {
+        icon: "mdi-shaker-outline",
+        title: "Epices",
+    },
+    "vegetables": {
+        title: "Fruits & Légumes",
+        icon: "mdi-fruit-cherries",
+    },
+};
--- src/views/shopping/ShoppingListView.vue
+++ src/views/shopping/ShoppingListView.vue
@@ -1,47 +1,81 @@
 <template>
-  <v-container>
+  <v-container style="position: relative">
     <h1>Liste de courses</h1>
-    <router-link :to="{ name: 'shopping-recipes' }">Recettes</router-link>
 
-    <v-list v-for="(items, shelve) in itemsByShelve" :key="shelve">
-      <v-subheader>{{ shelve }}</v-subheader>
+    <router-link
+      :to="{ name: 'shopping-recipes' }"
+    >
+      Modifier les recettes
+    </router-link>
 
-      <v-list-item
-        v-for="(item, i) in items"
-        :key="i"
-        :value="item"
-        color="primary"
-        variant="plain"
-        two-line
-        @click.capture.stop="taGueule(item)"
+    <div
+      style="
+        position: fixed;
+        left: 1em;
+        right: 1em;
+        bottom: 56px;
+        background: white;
+        z-index: 999;
+        padding: 1em;
+      "
+    >
+      <v-progress-linear color="primary" :value="countProgress" height="22">
+        <strong>{{ Math.ceil(countProgress) }} %</strong>
+      </v-progress-linear>
+    </div>
+
+    <v-list v-for="shelve in itemsByShelve" :key="shelve">
+      <v-list-group
+        :value="!shelve.checked"
+        :prepend-icon="shelve.icon"
       >
-        <v-list-item-action>
-          <v-checkbox :input-value="item.checked"></v-checkbox>
-        </v-list-item-action>
+        <template v-slot:activator>
+          <v-list-item-title :style="computeItemStyle(shelve)">
+            <strong>
+              {{ shelve.name }}
+            </strong>
+          </v-list-item-title>
+        </template>
 
-        <v-list-item-content>
-          <v-list-item-title>{{ item.ingredient.name }}</v-list-item-title>
-          <v-list-item-subtitle
-            >{{ item.quantity }} {{ item.unit }}</v-list-item-subtitle
-          >
-        </v-list-item-content>
-      </v-list-item>
+        <v-list-item
+          v-for="(item, i) in shelve.items"
+          :key="i"
+          :value="item"
+          color="primary"
+          variant="plain"
+          two-line
+          @click.capture.stop="toggleItemCheck(item)"
+        >
+          <v-list-item-content style="padding-left: 2em">
+            <v-list-item-title :style="computeItemStyle(item)">
+              {{ item.ingredient.name }}
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              {{ item.quantity }} {{ item.unit }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-action>
+            <v-checkbox :input-value="item.checked"></v-checkbox>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list-group>
     </v-list>
   </v-container>
 </template>
 
 <script>
 import { mapState } from "vuex";
+import SHELVES from "@/constants/shelves";
 
 export default {
   data: () => ({
     items: [],
+    shelves: SHELVES,
   }),
   computed: {
     ...mapState("groups", ["selectedGroup"]),
     itemsByShelve() {
-      console.log(this.items);
-
       const itemsByShelve = {};
       for (let item of this.items) {
         let shelveItems = itemsByShelve[item.ingredient.shelve];
@@ -50,7 +84,29 @@ export default {
         }
         shelveItems.push(item);
       }
-      return itemsByShelve;
+      const shelves = [];
+      let counter = 0;
+      for(let shelve of Object.keys(itemsByShelve)){
+        counter = 0;
+        for(let item of itemsByShelve[shelve]){
+          if(!item.checked){
+            counter++;
+          }
+        }
+        shelves.push({"id": shelve, "name": SHELVES[shelve].title, "icon": SHELVES[shelve].icon, "items": itemsByShelve[shelve], "checked": counter===0})
+      }
+      return shelves;
+    },
+    countProgress() {
+      let checkedCount = 0;
+      let totalCount = 0;
+      for (let item of this.items) {
+        if (item.checked) {
+          checkedCount++;
+        }
+        totalCount++;
+      }
+      return (100 * checkedCount) / totalCount;
     },
   },
   watch: {
@@ -83,8 +139,15 @@ export default {
     },
   },
   methods: {
-    taGueule(item) {
+    toggleItemCheck(item) {
       item.checked = !item.checked;
+    },
+    computeItemStyle(item) {
+      const style = {};
+      if (item.checked) {
+        style.textDecoration = "line-through";
+      }
+      return style;
     },
   },
 };
